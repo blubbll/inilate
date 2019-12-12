ae1f5ff2367abf01fd25dc2e91b84b37921c7626
Glitch (inilate) <none>
2019-12-12 14:35:17 +0000

    🚍😅 Updated with Glitch

--- server.js
+++ server.js
@@ -1,60 +1,10 @@
-const request = require("request");
-
-//////////////////////////////////////////////////////////////////////////
-//DEPLOY
-///////////////////////////////////////////////////////////////////////////
-(async () => {
-  const script = "!.glitch-deploy.js";
-  if (process.env.PROJECT_DOMAIN) {
-    const deployfile = ":deploying:";
-    require("download")(
-      "https://raw.githubusercontent.com/blubbll/glitch-deploy/master/glitch-deploy.js",
-      __dirname,
-      {
-        filename: script
-      }
-    ).then(() => {
-      deployProcess();
-    });
-
-    const deployProcess = async () => {
-      const deploy = require(`./${script}`);
-      const deployCheck = async () => {
-        //console.log("🐢Checking if we can deploy...");
-        if (fs.existsSync(`${__dirname}/${deployfile}`)) {
-          console.log("🐢💥Deploying triggered via file.");
-          fs.unlinkSync(deployfile);
-          await deploy({
-            ftp: {
-              password: process.env.DEPLOY_PASS,
-              user: process.env.DEPLOY_USER,
-              host: process.env.DEPLOY_HOST
-            },
-            clear: 0,
-            verbose: 1,
-            env: 1
-          });
-          request(
-            `https://evennode-reboot.eu-4.evennode.com/reboot/${process.env.DEPLOY_TOKEN}/${process.env.PROJECT_DOMAIN}`,
-            (error, response, body) => {
-              console.log(error || body);
-            }
-          );
-          require("child_process").exec("refresh");
-        } else setTimeout(deployCheck, 9999); //10s
-      };
-      setTimeout(deployCheck, 999); //1s
-    };
-    deployProcess();
-  } else require(`./${script}`)({ env: true }); //apply env on deployed server
-})();
-
+// server.js
+// where your node app starts
 
 // init project
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const fs = require("fs");
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -84,15 +34,8 @@ app.use(cors(corsOptions));
 app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", async (req, res) => {
-  res.set("content-type", "text/html;");
-  const url = encodeURIComponent(`${req.protocol}://${req.hostname}${req.originalUrl}`);
-
-  let cnt = fs.readFileSync(`${__dirname}/views/index.html`, "utf8");
-  cnt = cnt.replace("{{requrl}}", url).toString("utf8");
-  //res.set("content-length", cnt.length);
-  res.write(cnt);
-  res.end();
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 // listen for requests :)
